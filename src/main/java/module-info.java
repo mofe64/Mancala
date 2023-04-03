84acd5c9bdcd26cf8f6c0e73af36bdb391a46b3f
mofe <ogunbiyioladapo33@gmail.com>
2023-04-03 17:40:25 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.groupthree.mancala {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.groupthree.mancala to javafx.fxml;
+    exports com.groupthree.mancala;
+}
