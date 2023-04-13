21162545e6739374939caf7008ceea4406bcd84f
EnterYb <129081328+EnterYb@users.noreply.github.com>
2023-04-13 20:50:47 +0100

    Adding Controllers

---
+++ src/main/java/com/groupthree/mancala/controllers/PlayerDashboardController.java
@@ -0,0 +1,19 @@
+package com.groupthree.mancala.controllers;
+
+import javafx.fxml.FXML;
+
+public class PlayerDashboardController {
+
+    @FXML
+    protected void onNewGameButtonClick() {
+        //todo Code to handle new game here
+    }
+    @FXML
+    protected void onProfileButtonClick() {
+        //todo Code to handle Profile action here
+    }
+    @FXML
+    protected void onLeaderboardButtonClick() {
+        //todo Code to handle Leaderboard action here
+    }
+}
---
+++ src/main/java/com/groupthree/mancala/controllers/StartController.java
@@ -0,0 +1,16 @@
+package com.groupthree.mancala.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class StartController {
+
+    @FXML
+    protected void onLoginButtonClick() {
+        //todo Code to handle login  action here
+    }
+    @FXML
+    protected void onSignUpButtonClick() {
+        //todo Code to handle sign up action here
+    }
+}
