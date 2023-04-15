3fe25bd4556505515a556712574e2906bd1948bf
EnterYb <129081328+EnterYb@users.noreply.github.com>
2023-04-15 07:49:21 +0100

    Adding java doc to start controller

--- src/main/java/com/groupthree/mancala/controllers/StartController.java
+++ src/main/java/com/groupthree/mancala/controllers/StartController.java
@@ -2,14 +2,22 @@ package com.groupthree.mancala.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-
+/** StartController is a class that manages click actions on the start screen
+ * @author Yebo Ajakpo
+ * @version 1.0
+ */
 public class StartController {
 
     @FXML
+    /** On clicking the 'Login Button' this will capture the user login credentials,
+     * authenticate it before giving authorization
+     */
     protected void onLoginButtonClick() {
         //todo Code to handle login  action here
     }
     @FXML
+    /** On clicking the 'Sign Up Button' this will capture the user details and save it.
+     */
     protected void onSignUpButtonClick() {
         //todo Code to handle sign up action here
     }
