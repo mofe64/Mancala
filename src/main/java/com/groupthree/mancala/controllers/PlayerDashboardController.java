6dddbed04420d6e3eadd3229d1a2ff132768d804
EnterYb <129081328+EnterYb@users.noreply.github.com>
2023-04-15 07:48:25 +0100

    Adding Java doc to player dashboard

--- src/main/java/com/groupthree/mancala/controllers/PlayerDashboardController.java
+++ src/main/java/com/groupthree/mancala/controllers/PlayerDashboardController.java
@@ -1,18 +1,27 @@
 package com.groupthree.mancala.controllers;
 
 import javafx.fxml.FXML;
-
+/** PlayerDashboardController is a class that manages click actions on the player dashboard screen
+ * @author Yebo Ajakpo
+ * @version 1.0
+ */
 public class PlayerDashboardController {
 
     @FXML
+    /** On clicking the 'New Game Button' this will display the new game menu
+     */
     protected void onNewGameButtonClick() {
         //todo Code to handle new game here
     }
     @FXML
+    /** On clicking the 'Profile Button' this will display the player's profile screen
+     */
     protected void onProfileButtonClick() {
         //todo Code to handle Profile action here
     }
     @FXML
+    /** On clicking the 'Leader Board Button' this will display the Leader board screen
+     */
     protected void onLeaderboardButtonClick() {
         //todo Code to handle Leaderboard action here
     }
