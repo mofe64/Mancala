d461087df379099dc3d3550bdf965b623e2d84c0
Adebobola Oluwadara <adebobolaoluwadara@gmail.com>
2023-04-15 09:53:33 +0100

    build player UI

--- src/main/java/com/groupthree/mancala/HelloApplication.java
+++ src/main/java/com/groupthree/mancala/HelloApplication.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcome-view.fxml"));
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(HelloApplication.class.getResource("player-profile.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Group 3");
         stage.setScene(scene);
